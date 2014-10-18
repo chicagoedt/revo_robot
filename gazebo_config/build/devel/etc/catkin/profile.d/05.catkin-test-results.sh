43ca3156cbb63e54028f099589eac027c74ede3b
Krystian <krystian@krystian-MacBookPro.(none)>
2014-10-18 16:17:30 -0500

    added .gitignore for build and lib folder in gazebo_config.

--- gazebo_config/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/krystian/ros_edt/catkin_ws/src/gazebo_config/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
