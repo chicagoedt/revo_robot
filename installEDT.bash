04699c3fcd0077fa56e54cccc60b0c16924375d2
Krystian-L0g1x <krgebis@gmail.com>
2014-11-17 15:55:37 -0800

    Adding wget for gazebo meshes

--- installEDT.bash
+++ installEDT.bash
@@ -30,4 +30,67 @@ echo "Executing 'roscd'..."
 roscd
 echo "You should now be in the /devel/ folder."
 
+echo "Now grabbing scipio's gazebo meshes from the EDT wiki..."
+cd ..
+cd src/simulation/scipio_simulation
+mkdir meshes
+cd meshes
+
+wget http://wiki.chicagoedt.org/images/7/70/Back_left_wheel_link.STL.zip
+unzip Back_left_wheel_link.STL.zip
+rm Back_left_wheel_link.STL.zip
+
+wget http://wiki.chicagoedt.org/images/7/71/Base_link.DAE.zip
+unzip Base_link.DAE.zip
+rm Base_link.DAE.zip
+
+wget http://wiki.chicagoedt.org/images/9/9b/Front_left_wheel_link.STL.zip
+unzip Front_left_wheel_link.STL.zip
+rm Front_left_wheel_link.STL.zip
+
+wget http://wiki.chicagoedt.org/images/9/95/Back_right_wheel_link.DAE.zip
+unzip Back_right_wheel_link.DAE.zip
+rm Back_right_wheel_link.DAE.zip
+
+wget http://wiki.chicagoedt.org/images/a/aa/Back_right_wheel_link.STL.zip
+unzip Back_right_wheel_link.STL.zip
+rm Back_right_wheel_link.STL.zip
+
+wget http://wiki.chicagoedt.org/images/d/d0/Front_right_wheel_link.STL.zip
+unzip Front_right_wheel_link.STL.zip
+rm Front_right_wheel_link.STL.zip
+
+wget http://wiki.chicagoedt.org/images/d/d4/Front_left_wheel_link.DAE.zip
+unzip Front_left_wheel_link.DAE.zip
+rm Front_left_wheel_link.DAE.zip
+
+wget http://wiki.chicagoedt.org/images/1/10/Base_link.STL.zip
+unzip Base_link.STL.zip
+rm Base_link.STL.zip
+
+wget http://wiki.chicagoedt.org/images/f/fa/Front_right_wheel_link.DAE.zip
+unzip Front_right_wheel_link.DAE.zip
+rm Front_right_wheel_link.DAE.zip
+
+wget http://wiki.chicagoedt.org/images/5/5a/Back_left_wheel_link.DAE.zip
+unzip Back_left_wheel_link.DAE.zip
+rm Back_left_wheel_link.DAE.zip
+
+roscd scipio_simulation/..
+
+cd grass/materials/textures/
+wget http://wiki.chicagoedt.org/images/6/63/Igvc_lines_4000x4000cm.png
+mv Igvc_lines_4000x4000cm.png igvc_lines_4000x4000cm.png
+
+cd ../../meshes/
+mkdir images
+cd images
+wget http://wiki.chicagoedt.org/images/c/c8/0_Grass0081_L.jpg
+wget http://wiki.chicagoedt.org/images/c/c9/1_igvc_lines.png
+
+roscd scipio_simulation/..
+
+cp -r scipio_simulation ~/.gazebo/models/
+cp -r grass ~/.gazebo/models/
+
 echo "Finished Installing EDT's IGVC Repository."
