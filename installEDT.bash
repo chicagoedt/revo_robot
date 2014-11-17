ad49e28ab5acd673a281cd8c6b25e9fcef70883c
Krystian-L0g1x <krgebis@gmail.com>
2014-11-16 17:12:54 -0800

    Modified installEDT.bash so it modifies users local .git settings to check dependencies every time they update. This might be wrong because i think what i want is everytime they merge (i.e pulling from upstream)

--- installEDT.bash
+++ installEDT.bash
@@ -1,1 +1,10 @@
 rosdep install --from-paths ./ --ignore-src --rosdistro indigo
+
+cd .git/hooks
+echo "#!/bin/bash" >> post-receive
+echo "rosdep install --from-paths src --ignore-src --rosdistro indigo" >> post-receive
+echo "#!/bin/bash" >> post-update
+echo "rosdep install --from-paths src --ignore-src --rosdistro indigo" >> post-update
+sudo chmod +x post-receive
+sudo chmod +x post-update
+cd ../../
