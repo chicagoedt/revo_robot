b326596083d6543816bd12e65c410f08ce11293f
Basheer Subei <basheersubei@gmail.com>
2015-03-14 17:01:43 -0500

    backproject node uses new class, and fixes launch files

--- line_detection/src/backproject.py
+++ line_detection/src/backproject.py
@@ -2,41 +2,26 @@
 import sys
 import numpy as np
 import cv2
-import math
-#import time
 import rospy
-import sensor_msgs
-from sensor_msgs.msg import CompressedImage
-from sensor_msgs.msg import Image
-import rospkg
 from dynamic_reconfigure.server import Server
+from lane_detection import LaneDetection
 from line_detection.cfg import LineDetectionConfig
-from cv_bridge import CvBridge, CvBridgeError
 
 ###############################################################################
-## Chicago Engineering Design Team
-## Histogram Backprojection using Python OpenCV for autonomous robot Scipio
-##    (IGVC competition).
-## @author Basheer Subei
-## @email basheersubei@gmail.com
-#######################################################
+# Chicago Engineering Design Team
+# Backprojection filter using Python OpenCV for autonomous robot Scipio
+#    (IGVC competition).
+# @author Basheer Subei
+# @email basheersubei@gmail.com
 
 
-class backprojection:
-
-    node_name = "backproject_lanedetection"
-    namespace = rospy.get_namespace()
-    if namespace == "/":
-        namespace = ""
-
-    use_compressed_format = rospy.get_param(rospy.get_namespace() + node_name + "/use_compressed_format")
-    subscriber_image_topic = rospy.get_param(rospy.get_namespace() + node_name + "/subscriber_image_topic")
-    publisher_image_topic = rospy.get_param(rospy.get_namespace() + node_name + "/publisher_image_topic")
-    buffer_size = rospy.get_param(rospy.get_namespace() + node_name + "/buffer_size")
-    # this is where we define our variables in the class.
-    # these are changed dynamically using dynamic_reconfig and affect
-    # the image processing algorithm. A lot of these are not used in the
-    # current algorithm.
+class Backproject(LaneDetection):
+    image_height = 0
+    image_width = 0
+    roi_top_left_x = 0
+    roi_top_left_y = 0
+    roi_width = 2000
+    roi_height = 2000
 
     # hsv range of values to consider for backprojection
     hue_low = 20
@@ -46,197 +31,76 @@ class backprojection:
     saturation_high = 255
 
     backprojection_kernel_size = 5
+    histogram = np.zeros((180, 256), dtype=float)
 
-    image_height = 0
-    image_width = 0
-
-    roi_top_left_x = 0
-    roi_top_left_y = 0
-    roi_width = 2000
-    roi_height = 2000
-
-    histogram_dimension = rospy.get_param(rospy.get_namespace() + node_name + "/histogram_dimension")
-
-    # histogram = np.zeros((histogram_dimension,histogram_dimension,histogram_dimension))
-    histogram = np.zeros((180,256), dtype=float)
-
-    def __init__(self):
-
-        # initialize ROS stuff
-
-        # publisher for image of line pixels (only for debugging, not used in
-        # map)
-        self.line_image_pub = rospy.Publisher( self.namespace +
-                                              "/" + self.node_name +
-                                              self.publisher_image_topic +
-                                              '/compressed',
-                                              sensor_msgs.msg.CompressedImage,
-                                              queue_size=1)
-
-        if self.use_compressed_format:
-        # subscriber for ROS image topic
-            self.image_sub = rospy.Subscriber(self.subscriber_image_topic +
-                                             "/compressed",
-                                              CompressedImage, self.image_callback,
-                                              queue_size=1, buff_size=self.buffer_size)
-        else:
-            # use this for uncompressed raw format
-            self.image_sub = rospy.Subscriber(self.subscriber_image_topic,
-                                           Image,
-                                           self.image_callback, queue_size=1,
-                                           buff_size=self.buffer_size)
-
-
-        self.bridge = CvBridge()
-
-        # self.histogram = np.load(self.package_path + "/misc/training_images/histogram.txt.npy")
-        self.histogram = np.loadtxt(self.package_path + "/misc/training_images/histogram.txt")
+    def __init__(self, namespace, node_name):
+        LaneDetection.__init__(self, namespace, node_name)
+        self.histogram = np.loadtxt(
+            self.package_path + "/misc/training_images/histogram.txt"
+        )
         cv2.normalize(self.histogram, self.histogram, 0, 255, cv2.NORM_MINMAX)
+
+    # this is what gets called when an image is recieved
+    def image_callback(self, ros_image):
+
+        cv2_image = LaneDetection.ros_to_cv2_image(self, ros_image)
+
+        # run backprojection to remove grass
+        mask = self.get_backprojection_mask(cv2_image)
+        mask = np.dstack((mask, mask, mask))
+        # only include pixels from thresh
+        final_image = cv2.bitwise_and(cv2_image, mask)
+
+        final_image_message = LaneDetection.cv2_to_ros_message(
+            self, final_image
+        )
+        # publishes final image message in ROS format
+        self.line_image_pub.publish(final_image_message)
+    # end image_callback()
+
     # returns mask based on backprojection
     def get_backprojection_mask(self, image):
 
         # Convert BGR to HSV
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        ## begin HISTOGRAM BACKPROJECTION
+        # begin HISTOGRAM BACKPROJECTION
         # backprojection on 3d histogram
-        backproject = cv2.calcBackProject([hsv],
-                                  [0,1],
-                                  self.histogram,
-                                  [self.hue_low, self.hue_high, self.saturation_low, self.saturation_high],
-                                  1)
+        backproject = cv2.calcBackProject(
+            [hsv], [0, 1], self.histogram,
+            [
+                self.hue_low, self.hue_high,
+                self.saturation_low, self.saturation_high
+            ],
+            1
+        )
 
         dst = backproject
         # Now convolute with circular disc
-        disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (self.backprojection_kernel_size, self.backprojection_kernel_size))
+        disc = cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE,
+            (self.backprojection_kernel_size, self.backprojection_kernel_size)
+        )
         cv2.filter2D(dst, -1, disc, dst)  # do we need this convolution???
 
         # invert dst (because the backprojection chooses what we DON'T want)
         dst = 255 - dst
 
         return dst
-        ## end HISTOGRAM BACKPROJECTION
+        # end HISTOGRAM BACKPROJECTION
 
-    # this is what gets called when an image is recieved
-    def image_callback(self, image):
-
-        # convert ROS image to OpenCV image
-        img = ROS_to_CV2_image(image)
-
-        if img is None:
-            rospy.logerr("error! img is empty!")
-            return
-
-        self.image_height = img.shape[0]
-        self.image_width = img.shape[1]
-
-        roi = img[
-            self.roi_top_left_y:self.roi_top_left_y + self.roi_height,
-            self.roi_top_left_x:self.roi_top_left_x + self.roi_width,
-            :
-        ]
-
-        # in case roi settings aren't correct, just use the entire image
-        if roi.size <= 0:
-            rospy.logerr("Incorrect roi settings! Will use the entire image instead!")
-            roi = img
-
-        # run backprojection to remove grass
-
-        mask = self.get_backprojection_mask(roi)
-        mask = np.dstack((mask, mask, mask))
-        final_image = cv2.bitwise_and(roi, mask)  # only include pixels from thresh
-
-        #### Create CompressedImage to publish ####
-        final_image_message = CompressedImage()
-        final_image_message.header.stamp = rospy.Time.now()
-        final_image_message.format = "jpeg"
-        final_image_message.data = np.array(cv2.imencode(
-                                            '.jpg',
-                                            final_image)[1]).tostring()
-
-        # publishes image message with line pixels in it
-        self.line_image_pub.publish(final_image_message)
-
-    ## end image_callback()
-
-    def ROS_to_CV2_image(self, image):
-
-        # if we don't have a color image, return and throw error
-        if hasattr(image, 'encoding') and image.encoding == 'mono8':
-            rospy.logerr("error! image is not color!")
-            return
-
-        if self.use_compressed_format:
-            #### direct conversion from ROS CompressedImage to CV2 ####
-            np_arr = np.fromstring(image.data, np.uint8)
-            img = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
-        else:
-            #### direct conversion from ROS Image to CV2 ####
-            # first, we need to convert image from sensor_msgs/Image to numpy (or
-            # cv2). For this, we use cv_bridge
-            try:
-                # img = self.bridge.imgmsg_to_cv2(image, "bgr8")
-                img = self.bridge.imgmsg_to_cv2(image,
-                                                desired_encoding="passthrough")
-            except CvBridgeError, e:
-                rospy.logerr(e)
-
-        return img
-
-    def reconfigure_callback(self, config, level):
-
-        self.hue_low = config['hue_low']
-        self.hue_high = config['hue_high']
-        self.saturation_low = config['saturation_low']
-        self.saturation_high = config['saturation_high']
-
-        self.backprojection_kernel_size = config['backprojection_kernel_size']
-        
-        self.roi_top_left_x = config['roi_top_left_x']
-        self.roi_top_left_y = config['roi_top_left_y']
-        self.roi_width = config['roi_width']
-        self.roi_height = config['roi_height']
-
-        self.validate_parameters()
-
-        return config
-
-    # makes sure the parameters are valid and don't crash the
-    # openCV calls. Changes them to valid values if invalid.
-    def validate_parameters(self):
-        
-        # these parameters need validation:
-
-        # hue, saturation, and value parameters cannot have
-        # larger or equal low limits than high limits
-        if self.hue_low >= self.hue_high:
-            self.hue_low = self.hue_high - 1
-        if self.saturation_low >= self.saturation_high:
-            self.saturation_low = self.saturation_high - 1
-
-        # now check if ROI parameters are out of bounds
-        # only do this if image dimensions have been set
-        if self.image_width > 0 and self.image_height > 0:
-            if self.roi_width > self.image_width - self.roi_top_left_x:
-                self.roi_width = self.image_width - self.roi_top_left_x
-            if self.roi_top_left_x < 0:
-                self.roi_top_left_x = 0
-
-            if self.roi_height > self.image_height - self.roi_top_left_y:
-                self.roi_height = self.image_height - self.roi_top_left_y
-            if self.roi_top_left_y < 0:
-                self.roi_top_left_y = 0
-        # kernel size can be an odd number only
-        if self.backprojection_kernel_size % 2 == 0:
-            self.backprojection_kernel_size -= 1
 
 def main(args):
-    # create a backprojection object
-    bp = backprojection()
+    node_name = "backproject_lanedetection"
+    namespace = rospy.get_namespace()
+    if namespace == "/":
+        namespace = ""
 
-    # start the backproject node and start listening
-    rospy.init_node("backproject", anonymous=True)
+    # create a Backproject object
+    bp = Backproject(namespace, node_name)
+
+    # start the line_detector node and start listening
+    rospy.init_node("backproject_lanedetection", anonymous=True)
 
     # starts dynamic_reconfigure server
     srv = Server(LineDetectionConfig, bp.reconfigure_callback)
