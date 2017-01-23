07f71809f57589a751c9a803671b2a9f099231b7
Jeff Alan <JeffKo427@gmail.com>
2017-01-22 18:38:29 -0600

    Began implementing teleoperation via XBox controller

--- revo_teleop/src/revo_teleop_node.cpp
+++ revo_teleop/src/revo_teleop_node.cpp
@@ -1,6 +1,21 @@
 #include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <base_controller/XBox_Button_Msg.h>
 
 int main(int argc char **argv) {
+    // Initialize the ROS system and become a node.
     ros::init(argc, argv, "revo_teleop");
     ros::NodeHandle nh;
+
+    // Create a publisher object.
+    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>(
+            "TODO/cmd_vel", 1000);
+
+    // Tell joy which js device to listen to.
+    // TODO: Pass this as a param or find a way to set it automatically.
+    ros::Subscriber sub = nh.subscribe("joy", 1000, &rt_callback);
+
+    // Let ROS take over.
+    ros::spin();
 }
