a4596f7cae82780a043e8832325d6593e632d250
Jeff Alan <JeffKo427@gmail.com>
2017-01-22 19:12:14 -0600

    Realized that current approach was a dead end

--- revo_teleop/src/revo_teleop_node.cpp
+++ revo_teleop/src/revo_teleop_node.cpp
@@ -1,7 +1,11 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include <sensor_msgs/Joy.h>
-#include <base_controller/XBox_Button_Msg.h>
+
+int rt_callback(const sensor_msgs::Joy& joystick_input) {
+    // Button Index: http://wiki.ros.org/joy#Application
+    cmd_vel_
+
+}
 
 int main(int argc char **argv) {
     // Initialize the ROS system and become a node.
@@ -9,13 +13,18 @@ int main(int argc char **argv) {
     ros::NodeHandle nh;
 
     // Create a publisher object.
-    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>(
-            "TODO/cmd_vel", 1000);
+    ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>(
+            "cmd_vel", 1000);
 
     // Tell joy which js device to listen to.
     // TODO: Pass this as a param or find a way to set it automatically.
-    ros::Subscriber sub = nh.subscribe("joy", 1000, &rt_callback);
+    // nh.setParam("joy_node/dev", "/dev/input/js1");
+
+    // Create a subscriber object.
+    ros::Subscriber sub = nh.subscribe("/base_controller/command", 1000, &rt_callback);
 
     // Let ROS take over.
     ros::spin();
+
+    return 0;
 }
