c8060cdb5dda3c9f2d189356d9603bbec3e0d5f7
Jeff Alan <JeffKo427@gmail.com>
2017-01-25 15:27:54 -0600

    Populated lane_finding with outline of image processing pipeline

--- lane_finding/src/main.cpp
+++ lane_finding/src/main.cpp
@@ -1,4 +1,4 @@
-// This program does stuff
+// This program does all image processing.
 
 #include <ros/ros.h>
 #include <opencv_apps>
@@ -6,7 +6,17 @@
 #include <opencv_apps/LineArrayStamped.h>
 
 void findLanes(const sensor_msgs::CompressedImage frame) {
+    //TODO: Convert to grayscale
+    
+    //TODO: Gaussian blur to reduce noise using kernel size 3 or 5
 
+    //TODO: Canny edge detection
+
+    //TODO: Hough transform
+
+    //TODO: Delete everything outside the region of interest
+
+    //TODO: Perspective transform
 
 int main( int argc, char **argv ) {
     
@@ -15,8 +25,10 @@ int main( int argc, char **argv ) {
     ros::NodeHandle nh;
 
     // Create publisher and subscriber objects.
-    ros::Publisher lane_pub = nh.advertise<sensor_msgs::Image>("lane_detector/lane_lines", 1000);
-    ros::Subscriber hough_sub = nh.subscribe("opencv_apps/hough_lines`", 1000, &findLanes);
+    ros::Subscriber left_sub = nh.subscribe("/stereo_camera/left/image_color", 1000, &findLanes);
+    ros::Subscriber right_sub = nh.subscribe("/stereo_camera/right/image_color", 1000, &findLanes);
+    ros::Publisher left_pub = nh.advertise<sensor_msgs::Image>("lane_detector/lane_lines/left", 1000);
+    ros::Publisher left_pub = nh.advertise<sensor_msgs::Image>("lane_detector/lane_lines/right", 1000);
     
     // Let ROS take over.
     ros::spin();
