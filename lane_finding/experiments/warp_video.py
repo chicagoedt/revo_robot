a53fbc10b6a401064565e828d0b2a1e414ff5652
Jeff <JeffKo427@gmail.com>
2017-02-16 19:14:07 -0600

    Updated warp_video.py to, you know, warp videos

--- lane_finding/experiments/warp_video.py
+++ lane_finding/experiments/warp_video.py
@@ -2,15 +2,22 @@ import cv2
 import numpy as np
 import sys
 
-full_img = cv2.imread(sys.argv[1])
-height, width = full_img.shape[:2]
-img = cv2.resize(full_img, (width/4, height/4))
-rows,cols,ch = img.shape
+cap = cv2.VideoCapture(sys.argv[1])
+counter = 0
+
 pts1 = np.float32([[330,375],[460,370],[310,610],[450,610]])
 pts2 = np.float32([[330,375],[460,375],[330,689],[460,689]])
 M = cv2.getPerspectiveTransform(pts1,pts2)
-print M
-dst = cv2.warpPerspective(img,M,(width/4, height/4))
-cv2.imshow('img', img)
-cv2.imshow('dst', dst)
-cv2.waitKey(0)
+
+while True:
+    ret, frame = cap.read()
+    height, width = frame.shape[:2]
+    dst = cv2.warpPerspective(frame, M, (width, height))
+    cv2.imshow('original',frame)
+    cv2.imshow('warp', dst)
+
+    if cap.get(1) == cap.get(7):
+        cap.set(1,0)
+
+    if 0xFF & cv2.waitKey(5) == 27:
+        break
