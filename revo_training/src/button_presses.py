86700669f6b47b8ea2830a9a16d9e66109f9d571
Jeff <JeffKo427@gmail.com>
2017-02-16 19:44:24 -0600

    Created button_handler node for use with move.launch

---
+++ revo_training/src/button_presses.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import roslib
+#roslib.load_manifest('my_package')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+# TODO: Everything
