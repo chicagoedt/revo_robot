53bf4aaa4ba73116801dd3df107983d0fc47a5f1
Jeff <JeffKo427@gmail.com>
2017-02-27 13:34:11 -0600

    Eliminated most noise and wrote method for logging of contours which should be ignored.

--- lane_finding/experiments/lane_isolation.py
+++ lane_finding/experiments/lane_isolation.py
@@ -34,7 +34,26 @@ def thresh(img, tightThresh, wideThresh):
 
     return thresh_img
 
+def isWeird(contour):
+    convexHull = cv2.convexHull(contour)
+    clength = cv2.arcLength(contour, True)
+    hlength = cv2.arcLength(convexHull, True)
+    if clength / hlength > 2.5:
+        return True
+    else:
+        return False
 
+def getBadCountours(img, contours):
+    print "Hit 'd' if contour is bad, otherwise hit any other key."
+    for c in contours:
+        img = img * 0
+        cv2.drawContours(img, [c], -1, 255, -1)
+        cv2.imshow('Check', img)
+        kp = cv2.waitKey(0)
+        if 0xFF & kp == ord('d'):
+            print "YOU WILL BE DELETED"
+        elif 0xFF & kp == 27:
+            break
 
 
 cap = cv2.VideoCapture(sys.argv[1])
@@ -54,19 +73,33 @@ while True:
     sat_thresh = thresh( saturation, (30,80), (0,100) )
 
     athresh = cv2.adaptiveThreshold(saturation, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,33,12)
+    contours, hierarchy = cv2.findContours(athresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    bigContours = []
+    normalContours = []
+    for c in contours:
+        if cv2.contourArea(c) > 200:
+            bigContours.append(c)
+    for c in bigContours:
+        if not isWeird(c):
+            normalContours.append(c)
+
+    con = athresh.copy() * 0
+    cv2.drawContours(con, normalContours, -1, 255, -1)
 
     cv2.imshow( 'Original', frame )
-    cv2.imshow( 'Canny', canny )
-    #cv2.imshow( 'sat_thresh', sat_thresh)
-    cv2.imshow( 'Adaptive', athresh )
     cv2.imshow( 'Saturation', saturation)
+    cv2.imshow( 'Contoured', con )
 
     if cap.get(1) == cap.get(7): # Enums are broken, 1 is frame position, 7 is frame count
         cap.set(1, 0)
 
-
-    if 0xFF & cv2.waitKey(5) == 27:
+    press = cv2.waitKey(5)
+    if 0xFF & press == 27:
         break
+    if 0xFF & press == 32:
+        tmpimg = frame.copy() * 0
+        getBadCountours(tmpimg, normalContours)
+    press = None
 
 cap.release()
 cv2.destroyAllWindows()
