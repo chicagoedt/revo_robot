08e124f520fc431fb74422f7c15547085fa66a8a
Jeff Alan <JeffKo427@gmail.com>
2017-03-24 14:47:28 -0500

    Created script for generating custom VGG16 model

---
+++ revo_training/src/create_vgg16.py
@@ -0,0 +1,16 @@
+from keras.applications.vgg16 import VGG16
+from keras.layers import Flatten, Dropout, Dense
+from keras.utils import plot_model
+
+vgg = VGG16(weights='imagenet', include_top=False)
+
+flat = Flatten()(vgg.output)
+fc6 = Dense(1024, activation='relu', name='fc6')(flat)
+drop1 = Dropout(0.5)(fc6)
+fc7 = Dense(1024, activation='relu', name='fc7')(drop1)
+drop2 = Dropout(0.5)(fc7)
+classify = Dense(3, activation='softmax', name='classify')(drop2)
+
+model = Model(vgg.input, classify)
+model.save('VGG16-new.h5')
+plot_model(model, 'VGG16-new_plot.png')
