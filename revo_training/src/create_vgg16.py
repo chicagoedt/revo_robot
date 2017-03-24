eda9efe8ff5e094011eaeddd8e057503728dffec
Jeff Alan <JeffKo427@gmail.com>
2017-03-24 15:22:55 -0500

    Finalized create_vgg16.py, added *.h5 to gitignore

--- revo_training/src/create_vgg16.py
+++ revo_training/src/create_vgg16.py
@@ -1,8 +1,9 @@
+from keras.models import Model
 from keras.applications.vgg16 import VGG16
 from keras.layers import Flatten, Dropout, Dense
 from keras.utils import plot_model
 
-vgg = VGG16(weights='imagenet', include_top=False)
+vgg = VGG16(weights='imagenet', include_top=False, input_shape=(224,224,3))
 
 flat = Flatten()(vgg.output)
 fc6 = Dense(1024, activation='relu', name='fc6')(flat)
@@ -13,4 +14,4 @@ classify = Dense(3, activation='softmax', name='classify')(drop2)
 
 model = Model(vgg.input, classify)
 model.save('VGG16-new.h5')
-plot_model(model, 'VGG16-new_plot.png')
+plot_model(model, 'VGG16-new_plot.png', show_shapes=True)
