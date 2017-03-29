b0aac37813d3e9f46b3f444bc9c08edc9d4dd7ac
Jeff Alan <JeffKo427@gmail.com>
2017-03-28 20:24:08 -0500

    created yet another script for creating and training yet another model

---
+++ revo_training/src/create_and_train.py
@@ -0,0 +1,42 @@
+from keras.applications.vgg16 import VGG16
+from keras.models import Sequential
+from keras.utils import plot_model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, UpSampling2D
+
+#vgg = VGG16(include_top=False, weights='imagenet', input_shape=input_shape)
+
+def buildModel():
+    model = Sequential()
+
+    model.add(Conv2D(64, (3,3), padding='same', activation='relu', input_shape=(224,224,3)))
+    model.add(Conv2D(64, (3,3), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+
+    model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3,3), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+
+    model.add(Conv2D(256, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3,3), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2,2)))
+
+    model.add(Conv2D(512, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(512, (3,3), padding='same', activation='relu'))
+    model.add(Conv2D(512, (3,3), padding='same', activation='relu'))
+
+    model.add(Conv2D(512, (3,3), padding='same', activation='relu', dilation_rate=2))
+    model.add(Conv2D(512, (3,3), padding='same', activation='relu', dilation_rate=2))
+    model.add(Conv2D(512, (3,3), padding='same', activation='relu', dilation_rate=2))
+
+    model.add(Conv2D(4096, (7,7), padding='same', dilation_rate=4))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(4096, (1,1), padding='same', activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(2, (1,1), padding='same'))
+
+    model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    plot_model(model, 'dilated.png', show_shapes=True)
+
+    return model
