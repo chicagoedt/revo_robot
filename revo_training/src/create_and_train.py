b75fa5876cf3028c164a4058f6a0e73da426e4d2
Jeff Alan <JeffKo427@gmail.com>
2017-04-11 16:36:29 -0500

    implemented enet

--- revo_training/src/create_and_train.py
+++ revo_training/src/create_and_train.py
@@ -1,23 +1,27 @@
 from keras.applications.vgg16 import VGG16
-from keras.models import Sequential, load_model
+from keras.models import Sequential, Model, load_model
 from keras.utils import plot_model
-from keras.layers import Conv2D, MaxPooling2D, Dropout, UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D, Dropout, UpSampling2D, Input, BatchNormalization, add, concatenate, PReLU, Add, Conv2DTranspose
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
+from keras import backend as K
 import cv2
 import string, random
+import numpy as np
+import tensorflow as tf
 
 # HYPERPARAMETERS
 img_height = 224
 img_width = 224
 img_size = (img_height, img_width)
-mask_size = (28,28)
+mask_size = (224,224)
 input_shape = (img_height, img_width, 3)
-batch_size = 8
+batch_size = 1
 epochs = 500
 steps_per_epoch = int(1406/batch_size) + 1
 validation_steps = int(361/batch_size) + 1
 seed = 1
+internal_scale = 0.5
 
 vgg = VGG16(include_top=False, weights='imagenet', input_shape=input_shape)
 plot_model(vgg, 'vgg.png', show_shapes=True)
@@ -36,51 +40,82 @@ def zip3(*iterables):
             result.append(elem)
         yield tuple(result)
 
+def addBottleneck(model, output, downsampling=False, upsampling=False, asymmetric=False, dilation_rate=(1,1)):
+    if downsampling:
+        shape = (batch_size, int(int(model.shape[1]) / 2), int(int(model.shape[2]) / 2), int(output - int(model.shape[3])))
+        x = tf.convert_to_tensor(np.zeros(shape))
+        main = Conv2D(output, (shape[1],shape[2]), padding='same')(MaxPooling2D()(model))
+        projection = PReLU()(BatchNormalization()(Conv2D(int(output * internal_scale), (2,2), strides=(2,2), padding='same')(model)))
+    elif upsampling:
+        main = Conv2D(output, (int(model.shape[1]), int(model.shape[2])), padding='same')(UpSampling2D()(model))
+        projection = PReLU()(BatchNormalization()(Conv2D(int(output * internal_scale), (1,1), padding='same')(model)))
+    else:
+        main = model
+        projection = PReLU()(BatchNormalization()(Conv2D(int(output * internal_scale), (1,1), padding='same')(model)))
+
+    if asymmetric:
+        conv = PReLU()(BatchNormalization()(Conv2D(int(output * internal_scale), (5,1), padding='same')(Conv2D(int(output * internal_scale), (1,5), padding='same')(projection))))
+    else:
+        conv = PReLU()(BatchNormalization()(Conv2D(int(output * internal_scale), (3,3), padding='same', dilation_rate=dilation_rate)(projection)))
+
+    if upsampling:
+        #expansion = PReLU()(BatchNormalization()(Conv2DTranspose(output, (1,1), strides=(2,2), padding='same')(conv)))
+        expansion = UpSampling2D()(PReLU()(BatchNormalization()(Conv2D(output, (1,1), padding='same')(conv))))
+    else:
+        expansion = PReLU()(BatchNormalization()(Conv2D(output, (1,1), padding='same')(conv)))
+    conv_branch = Dropout(0.1)(expansion)
+
+    out = PReLU()(add([conv_branch, main]))
+
+    return out
 
 def buildModel():
-    model = Sequential()
+    i = Input(shape=input_shape)
+    c = Conv2D(13, (3,3), strides=(2,2), padding='same')(i)
+    p = MaxPooling2D()(i)
+    initial_block_output = concatenate([c,p])
 
-    model.add(Conv2D(64, (3,3), padding='same', activation='relu', name='block1_conv1', input_shape=(224,224,3)))
-    model.add(Conv2D(64, (3,3), padding='same', activation='relu', name='block1_conv2'))
-    model.add(MaxPooling2D((2,2)))
+    bottleneck_1_0 = addBottleneck(initial_block_output, 64, downsampling=True)
+    bottleneck_1_1 = addBottleneck(bottleneck_1_0, 64)
+    bottleneck_1_2 = addBottleneck(bottleneck_1_1, 64)
+    bottleneck_1_3 = addBottleneck(bottleneck_1_2, 64)
+    bottleneck_1_4 = addBottleneck(bottleneck_1_3, 64)
 
-    model.add(Conv2D(128, (3,3), padding='same', activation='relu', name='block2_conv1'))
-    model.add(Conv2D(128, (3,3), padding='same', activation='relu', name='block2_conv2'))
-    model.add(MaxPooling2D((2,2)))
+    bottleneck_2_0 = addBottleneck(bottleneck_1_4, 128, downsampling=True)
+    bottleneck_2_1 = addBottleneck(bottleneck_2_0, 128)
+    bottleneck_2_2 = addBottleneck(bottleneck_2_1, 128, dilation_rate=(2,2))
+    bottleneck_2_3 = addBottleneck(bottleneck_2_2, 128, asymmetric=True)
+    bottleneck_2_4 = addBottleneck(bottleneck_2_3, 128, dilation_rate=(4,4))
+    bottleneck_2_5 = addBottleneck(bottleneck_2_4, 128)
+    bottleneck_2_6 = addBottleneck(bottleneck_2_5, 128, dilation_rate=(8,8))
+    bottleneck_2_7 = addBottleneck(bottleneck_2_6, 128, asymmetric=True)
+    bottleneck_2_8 = addBottleneck(bottleneck_2_7, 128, dilation_rate=(16,16))
 
-    model.add(Conv2D(256, (3,3), padding='same', activation='relu', name='block3_conv1'))
-    model.add(Conv2D(256, (3,3), padding='same', activation='relu', name='block3_conv2'))
-    model.add(Conv2D(256, (3,3), padding='same', activation='relu', name='block3_conv3'))
-    model.add(MaxPooling2D((2,2)))
+    bottleneck_3_1 = addBottleneck(bottleneck_2_8, 128)
+    bottleneck_3_2 = addBottleneck(bottleneck_3_1, 128, dilation_rate=(2,2))
+    bottleneck_3_3 = addBottleneck(bottleneck_3_2, 128, asymmetric=True)
+    bottleneck_3_4 = addBottleneck(bottleneck_3_3, 128, dilation_rate=(4,4))
+    bottleneck_3_5 = addBottleneck(bottleneck_3_4, 128)
+    bottleneck_3_6 = addBottleneck(bottleneck_3_5, 128, dilation_rate=(8,8))
+    bottleneck_3_7 = addBottleneck(bottleneck_3_6, 128, asymmetric=True)
+    bottleneck_3_8 = addBottleneck(bottleneck_3_7, 128, dilation_rate=(16,16))
 
-    model.add(Conv2D(512, (3,3), padding='same', activation='relu', name='block4_conv1'))
-    model.add(Conv2D(512, (3,3), padding='same', activation='relu', name='block4_conv2'))
-    model.add(Conv2D(512, (3,3), padding='same', activation='relu', name='block4_conv3'))
+    bottleneck_4_0 = addBottleneck(bottleneck_3_8, 64, upsampling=True)
+    bottleneck_4_1 = addBottleneck(bottleneck_4_0, 64)
+    bottleneck_4_2 = addBottleneck(bottleneck_4_1, 64)
 
-    model.add(Conv2D(512, (3,3), padding='same', activation='relu', name='block5_conv1', dilation_rate=2))
-    model.add(Conv2D(512, (3,3), padding='same', activation='relu', name='block5_conv2', dilation_rate=2))
-    model.add(Conv2D(512, (3,3), padding='same', activation='relu', name='block5_conv3', dilation_rate=2))
+    bottleneck_5_0 = addBottleneck(bottleneck_4_2, 32, upsampling=True)
+    bottleneck_5_1 = addBottleneck(bottleneck_5_0, 32)
 
-    model.add(Conv2D(4096, (7,7), padding='same', activation='relu', name='conv6', dilation_rate=4))
-    model.add(Dropout(0.5))
-    model.add(Conv2D(4096, (1,1), padding='same', activation='relu', name='conv7'))
-    model.add(Dropout(0.5))
-    model.add(Conv2D(1, (1,1), padding='same', activation='sigmoid', name='pred'))
+    fullconv = Conv2D(1, (112,112))(bottleneck_5_1)
 
-    #model.add(UpSampling2D(size=(8,8)))
+    return Model(inputs=i, outputs=fullconv)
 
-    model.load_weights('vgg16_imagenet_weights.h5', by_name=True)
-    for layer in model.layers:
-        if layer.name == 'block5_conv1':
-            break
-        else:
-            layer.trainable = False
+model = buildModel()
+#plot_model(model, 'enet.png', show_shapes=True)
+model.compile(loss='binary_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
-    model.compile(optimizer='adadelta', loss='mean_squared_error', metrics=['accuracy'])
 
-    plot_model(model, 'dilated.png', show_shapes=True)
-
-    return model
 
 data_gen_args = dict(rotation_range=30.,
                      width_shift_range=0.2,
@@ -143,7 +178,7 @@ tb = TensorBoard(
 early = EarlyStopping(patience=3, verbose=1)
 
 #model = buildModel()
-model = load_model('best.h5')
+#model = load_model('best.h5')
 
 model.fit_generator(
         train_generator,
